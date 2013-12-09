c35072a1def702085652c9c90c71251cb8c820d4
Jeffrey Sykes <sykesjs@gmail.com>
2013-12-09 14:59:59 -0600

    Adding Methods

--- Jira.psm1
+++ Jira.psm1
@@ -1,7 +1,13 @@
-﻿$jira_api_base = "https://jira.isg.co.uk/rest/api/latest/"
-
-Function ConvertTo-SafeUri($uri) {
+﻿Function ConvertTo-SafeUri($uri) {
     Return [System.Uri]::EscapeDataString($uri)
+}
+
+Function Set-JiraApiBase {
+    Param (
+        [Parameter (Mandatory=$True)]
+        [string] $jira_api_base
+
+        $env:JIRA_API_BASE = $jira_api_base
 }
 
 Function Set-JiraCredentials {
@@ -17,11 +23,13 @@ Function Set-JiraCredentials {
 }
 
 Function Invoke-JiraRequest($method, $request) {
+    If ($env:JIRA_API_BASE -eq $Null) {
+        Write-Error "JIRA API Base has not been set, please run ``Set-JiraApiBase'"
     If ($env:JIRA_CREDENTIALS -eq $Null) {
         Write-Error "No JIRA credentials have been set, please run ``Set-JiraCredentials'"
     }
-    Write-Debug "Calling $method $jira_api_base$request with AUTH: Basic $env:JIRA_CREDENTIALS"
-    Return Invoke-RestMethod -Uri "${jira_api_base}${request}" -Headers @{"AUTHORIZATION"="Basic $env:JIRA_CREDENTIALS"} -Method $method
+    Write-Debug "Calling $method $env:JIRA_API_BASE$request with AUTH: Basic $env:JIRA_CREDENTIALS"
+    Return Invoke-RestMethod -Uri "$env:JIRA_API_BASE${request}" -Headers @{"AUTHORIZATION"="Basic $env:JIRA_CREDENTIALS"} -Method $method
 }
 
 Function Get-JiraIssue($issue) {
@@ -36,9 +44,16 @@ Function Get-JiraSearchResult($query) {
     Return Invoke-JiraRequest GET "search?jql=$(ConvertTo-SafeUri $query)"
 }
 
+Function Get-JiraProjectList {
+    Return Invoke-JiraRequest GET "project"
+}
+
+Function Get
+
 Export-ModuleMember -Function Set-JiraCredentials,
                               ConvertTo-SafeUri,
-                              Invoke-JiraQuery,
+                              Invoke-JiraRequest,
+                              Get-JiraProjectList,
                               Get-JiraIssue,
                               Get-JiraHistory,
                               Get-JiraSearchResult
