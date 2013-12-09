f844877c90c25c5f9549da12320b4c6af6ec2268
Jeffrey Sykes <sykesjs@gmail.com>
2013-12-09 16:36:08 -0600

    Added additional Functions: Get-JiraProjectRole Start-JiraBackgroundIndex
    Started commenting functions

---
+++ Add-JiraGroupToProjects.ps1
@@ -0,0 +1,33 @@
+﻿<#
+vim: syntax=ps1:ts=3
+#>
+
+<#
+.SYNOPSIS
+Adds Jira Group to All Projects on a Server
+
+.DESCRIPTION
+This Script will allow you to add a Group to all Projects on a Jira Server.
+
+.NOTES
+$Id:  $
+$URL:  $
+
+.EXAMPLE
+Add-JiraGroupToProjects
+
+.LINK
+https://github.com/MajorManUMan/jira-ps
+#>
+
+
+# Library Import
+
+# Functions
+
+# Constants
+
+# Variables
+
+
+# Main Script Body
